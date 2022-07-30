05b91a91e7b0d876405ff20e74c3b75e2486e579
Kimberly Ramos <kimberlyramos45980@yahoo.com>
2022-07-30 16:05:19 -0700

    Buttons open AddCustomer form/Validates inputted email with database

--- PizzaShopApp/Form1.cs
+++ PizzaShopApp/Form1.cs
@@ -9,7 +9,23 @@ namespace PizzaShopApp
 
         private void BtnCreateAccount_Click(object sender, EventArgs e)
         {
+            AddCustomer addCustomerForm = new();
+            DialogResult result = addCustomerForm.ShowDialog();
+        }
 
+        private void BtnLogin_Click(object sender, EventArgs e)
+        {
+            using PizzaShopContext dbContext = new();
+
+            string email = TxtCustomerEmail.Text;
+
+            bool doesExist = (from c in dbContext.Customers
+                              where c.EmailAddress == email
+                              select c).Any();
+            if (!doesExist)
+            {
+                LbEmailError.Visible = true;
+            }
         }
     }
 }
