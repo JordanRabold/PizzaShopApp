da298ad1051dd4f0fd4e4932d1489705552162f7
Jordan Rabold <97548765+JordanRabold@users.noreply.github.com>
2022-08-08 14:59:59 -0700

    Created home page with account button

--- PizzaShopApp/HomePage.Designer.cs
+++ PizzaShopApp/HomePage.Designer.cs
@@ -28,12 +28,46 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.btnAccountInfo = new System.Windows.Forms.Button();
+            this.labelUserName = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // btnAccountInfo
+            // 
+            this.btnAccountInfo.Location = new System.Drawing.Point(694, 12);
+            this.btnAccountInfo.Name = "btnAccountInfo";
+            this.btnAccountInfo.Size = new System.Drawing.Size(94, 29);
+            this.btnAccountInfo.TabIndex = 0;
+            this.btnAccountInfo.Text = "Account";
+            this.btnAccountInfo.UseVisualStyleBackColor = true;
+            this.btnAccountInfo.Click += new System.EventHandler(this.btnAccountInfo_Click);
+            // 
+            // labelUserName
+            // 
+            this.labelUserName.AutoSize = true;
+            this.labelUserName.Location = new System.Drawing.Point(29, 21);
+            this.labelUserName.Name = "labelUserName";
+            this.labelUserName.Size = new System.Drawing.Size(0, 20);
+            this.labelUserName.TabIndex = 1;
+            // 
+            // HomePage
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 20F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Text = "HomePage";
+            this.Controls.Add(this.labelUserName);
+            this.Controls.Add(this.btnAccountInfo);
+            this.Name = "HomePage";
+            this.Text = "Home";
+            this.Load += new System.EventHandler(this.HomePage_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private Button btnAccountInfo;
+        private Label labelUserName;
     }
 }
