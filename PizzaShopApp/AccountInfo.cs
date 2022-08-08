3d3174e00e61e69a1c3e2b4d9cc22dea67bb3208
Jordan Rabold <97548765+JordanRabold@users.noreply.github.com>
2022-08-08 14:58:53 -0700

    Created account info page and displayed all accounts

---
+++ PizzaShopApp/AccountInfo.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.Data.SqlClient;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace PizzaShopApp
+{
+    public partial class AccountInfo : Form
+    {
+        public AccountInfo()
+        {
+            InitializeComponent();
+        }
+
+        private void AccountInfo_Load(object sender, EventArgs e)
+        {
+            using PizzaShopContext dbContext = new();
+            dataGridView1.DataSource = dbContext.Customers.ToList();
+        }
+    }
+}
