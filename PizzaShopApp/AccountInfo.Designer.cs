2a34c1a6283491a01a6a324df9203698fb810a60
Kimberly Ramos <kimberlyramos45980@yahoo.com>
2022-08-09 12:35:01 -0700

    Displays Customer account info in Account Form

--- PizzaShopApp/AccountInfo.Designer.cs
+++ PizzaShopApp/AccountInfo.Designer.cs
@@ -33,18 +33,18 @@
             this.dataGridView1 = new System.Windows.Forms.DataGridView();
             this.label1 = new System.Windows.Forms.Label();
             this.label2 = new System.Windows.Forms.Label();
-            this.textBox1 = new System.Windows.Forms.TextBox();
-            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.TxtFirstName = new System.Windows.Forms.TextBox();
+            this.TxtLastName = new System.Windows.Forms.TextBox();
             this.label3 = new System.Windows.Forms.Label();
             this.label4 = new System.Windows.Forms.Label();
             this.label5 = new System.Windows.Forms.Label();
             this.label6 = new System.Windows.Forms.Label();
             this.label7 = new System.Windows.Forms.Label();
-            this.textBox3 = new System.Windows.Forms.TextBox();
-            this.textBox4 = new System.Windows.Forms.TextBox();
-            this.textBox5 = new System.Windows.Forms.TextBox();
-            this.textBox6 = new System.Windows.Forms.TextBox();
-            this.textBox7 = new System.Windows.Forms.TextBox();
+            this.TxtEmail = new System.Windows.Forms.TextBox();
+            this.TxtStreetAddress = new System.Windows.Forms.TextBox();
+            this.TxtCity = new System.Windows.Forms.TextBox();
+            this.TxtZipCode = new System.Windows.Forms.TextBox();
+            this.TxtState = new System.Windows.Forms.TextBox();
             ((System.ComponentModel.ISupportInitialize)(this.customerBindingSource)).BeginInit();
             ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
             this.SuspendLayout();
@@ -84,21 +84,21 @@
             this.label2.TabIndex = 4;
             this.label2.Text = "Last Name:";
             // 
-            // textBox1
+            // TxtFirstName
             // 
-            this.textBox1.Location = new System.Drawing.Point(256, 35);
-            this.textBox1.Margin = new System.Windows.Forms.Padding(6);
-            this.textBox1.Name = "textBox1";
-            this.textBox1.Size = new System.Drawing.Size(602, 47);
-            this.textBox1.TabIndex = 5;
+            this.TxtFirstName.Location = new System.Drawing.Point(256, 35);
+            this.TxtFirstName.Margin = new System.Windows.Forms.Padding(6);
+            this.TxtFirstName.Name = "TxtFirstName";
+            this.TxtFirstName.Size = new System.Drawing.Size(602, 47);
+            this.TxtFirstName.TabIndex = 5;
             // 
-            // textBox2
+            // TxtLastName
             // 
-            this.textBox2.Location = new System.Drawing.Point(256, 110);
-            this.textBox2.Margin = new System.Windows.Forms.Padding(6);
-            this.textBox2.Name = "textBox2";
-            this.textBox2.Size = new System.Drawing.Size(602, 47);
-            this.textBox2.TabIndex = 6;
+            this.TxtLastName.Location = new System.Drawing.Point(256, 110);
+            this.TxtLastName.Margin = new System.Windows.Forms.Padding(6);
+            this.TxtLastName.Name = "TxtLastName";
+            this.TxtLastName.Size = new System.Drawing.Size(602, 47);
+            this.TxtLastName.TabIndex = 6;
             // 
             // label3
             // 
@@ -145,58 +145,58 @@
             this.label7.TabIndex = 11;
             this.label7.Text = "Zip Code:";
             // 
-            // textBox3
+            // TxtEmail
             // 
-            this.textBox3.Location = new System.Drawing.Point(256, 196);
-            this.textBox3.Name = "textBox3";
-            this.textBox3.Size = new System.Drawing.Size(602, 47);
-            this.textBox3.TabIndex = 12;
+            this.TxtEmail.Location = new System.Drawing.Point(256, 196);
+            this.TxtEmail.Name = "TxtEmail";
+            this.TxtEmail.Size = new System.Drawing.Size(602, 47);
+            this.TxtEmail.TabIndex = 12;
             // 
-            // textBox4
+            // TxtStreetAddress
             // 
-            this.textBox4.Location = new System.Drawing.Point(256, 357);
-            this.textBox4.Name = "textBox4";
-            this.textBox4.Size = new System.Drawing.Size(602, 47);
-            this.textBox4.TabIndex = 13;
+            this.TxtStreetAddress.Location = new System.Drawing.Point(256, 357);
+            this.TxtStreetAddress.Name = "TxtStreetAddress";
+            this.TxtStreetAddress.Size = new System.Drawing.Size(602, 47);
+            this.TxtStreetAddress.TabIndex = 13;
             // 
-            // textBox5
+            // TxtCity
             // 
-            this.textBox5.Location = new System.Drawing.Point(119, 436);
-            this.textBox5.Name = "textBox5";
-            this.textBox5.Size = new System.Drawing.Size(316, 47);
-            this.textBox5.TabIndex = 14;
+            this.TxtCity.Location = new System.Drawing.Point(119, 436);
+            this.TxtCity.Name = "TxtCity";
+            this.TxtCity.Size = new System.Drawing.Size(316, 47);
+            this.TxtCity.TabIndex = 14;
             // 
-            // textBox6
+            // TxtZipCode
             // 
-            this.textBox6.Location = new System.Drawing.Point(1137, 436);
-            this.textBox6.Name = "textBox6";
-            this.textBox6.Size = new System.Drawing.Size(316, 47);
-            this.textBox6.TabIndex = 15;
+            this.TxtZipCode.Location = new System.Drawing.Point(1137, 436);
+            this.TxtZipCode.Name = "TxtZipCode";
+            this.TxtZipCode.Size = new System.Drawing.Size(316, 47);
+            this.TxtZipCode.TabIndex = 15;
             // 
-            // textBox7
+            // TxtState
             // 
-            this.textBox7.Location = new System.Drawing.Point(587, 436);
-            this.textBox7.Name = "textBox7";
-            this.textBox7.Size = new System.Drawing.Size(316, 47);
-            this.textBox7.TabIndex = 16;
+            this.TxtState.Location = new System.Drawing.Point(587, 436);
+            this.TxtState.Name = "TxtState";
+            this.TxtState.Size = new System.Drawing.Size(316, 47);
+            this.TxtState.TabIndex = 16;
             // 
             // AccountInfo
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(17F, 41F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(1615, 557);
-            this.Controls.Add(this.textBox7);
-            this.Controls.Add(this.textBox6);
-            this.Controls.Add(this.textBox5);
-            this.Controls.Add(this.textBox4);
-            this.Controls.Add(this.textBox3);
+            this.Controls.Add(this.TxtState);
+            this.Controls.Add(this.TxtZipCode);
+            this.Controls.Add(this.TxtCity);
+            this.Controls.Add(this.TxtStreetAddress);
+            this.Controls.Add(this.TxtEmail);
             this.Controls.Add(this.label7);
             this.Controls.Add(this.label6);
             this.Controls.Add(this.label5);
             this.Controls.Add(this.label4);
             this.Controls.Add(this.label3);
-            this.Controls.Add(this.textBox2);
-            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.TxtLastName);
+            this.Controls.Add(this.TxtFirstName);
             this.Controls.Add(this.label2);
             this.Controls.Add(this.label1);
             this.Controls.Add(this.dataGridView1);
@@ -216,17 +216,17 @@
         private DataGridView dataGridView1;
         private Label label1;
         private Label label2;
-        private TextBox textBox1;
-        private TextBox textBox2;
+        private TextBox TxtFirstName;
+        private TextBox TxtLastName;
         private Label label3;
         private Label label4;
         private Label label5;
         private Label label6;
         private Label label7;
-        private TextBox textBox3;
-        private TextBox textBox4;
-        private TextBox textBox5;
-        private TextBox textBox6;
-        private TextBox textBox7;
+        private TextBox TxtEmail;
+        private TextBox TxtStreetAddress;
+        private TextBox TxtCity;
+        private TextBox TxtZipCode;
+        private TextBox TxtState;
     }
 }
--- PizzaShopApp/AccountInfo.cs
+++ PizzaShopApp/AccountInfo.cs
@@ -22,8 +22,19 @@ namespace PizzaShopApp
 
         private void AccountInfo_Load(object sender, EventArgs e)
         {
+            // Display all the account info within their respctive Textboxes
+            TxtFirstName.Text = customerAccount.FirstName;
+            TxtLastName.Text = customerAccount.LastName;
+            TxtEmail.Text = customerAccount.EmailAddress;
+            TxtStreetAddress.Text = customerAccount.StreetAddress;
+            TxtState.Text = customerAccount.State;
+            TxtCity.Text = customerAccount.City;
+            TxtZipCode.Text = customerAccount.ZipCode;
+
             //using PizzaShopContext dbContext = new();
             //dataGridView1.DataSource = dbContext.Customers.ToList();
         }
+
+
     }
 }
