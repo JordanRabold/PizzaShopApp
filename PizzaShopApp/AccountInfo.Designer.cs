da6f21331a05a84c243fff1a4cfd2f90c2489435
Jordan Rabold <97548765+JordanRabold@users.noreply.github.com>
2022-08-15 08:46:55 -0700

    Added Customer ID label to AccountInfo form

--- PizzaShopApp/AccountInfo.Designer.cs
+++ PizzaShopApp/AccountInfo.Designer.cs
@@ -47,6 +47,7 @@
             this.TxtState = new System.Windows.Forms.TextBox();
             this.buttonDeleteAccount = new System.Windows.Forms.Button();
             this.label8 = new System.Windows.Forms.Label();
+            this.label9 = new System.Windows.Forms.Label();
             ((System.ComponentModel.ISupportInitialize)(this.customerBindingSource)).BeginInit();
             ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
             this.SuspendLayout();
@@ -68,7 +69,7 @@
             // label1
             // 
             this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(15, 20);
+            this.label1.Location = new System.Drawing.Point(15, 42);
             this.label1.Name = "label1";
             this.label1.Size = new System.Drawing.Size(83, 20);
             this.label1.TabIndex = 3;
@@ -77,7 +78,7 @@
             // label2
             // 
             this.label2.AutoSize = true;
-            this.label2.Location = new System.Drawing.Point(15, 57);
+            this.label2.Location = new System.Drawing.Point(15, 88);
             this.label2.Name = "label2";
             this.label2.Size = new System.Drawing.Size(82, 20);
             this.label2.TabIndex = 4;
@@ -85,14 +86,14 @@
             // 
             // TxtFirstName
             // 
-            this.TxtFirstName.Location = new System.Drawing.Point(120, 17);
+            this.TxtFirstName.Location = new System.Drawing.Point(120, 39);
             this.TxtFirstName.Name = "TxtFirstName";
             this.TxtFirstName.Size = new System.Drawing.Size(285, 27);
             this.TxtFirstName.TabIndex = 5;
             // 
             // TxtLastName
             // 
-            this.TxtLastName.Location = new System.Drawing.Point(120, 54);
+            this.TxtLastName.Location = new System.Drawing.Point(120, 85);
             this.TxtLastName.Name = "TxtLastName";
             this.TxtLastName.Size = new System.Drawing.Size(285, 27);
             this.TxtLastName.TabIndex = 6;
@@ -100,7 +101,7 @@
             // label3
             // 
             this.label3.AutoSize = true;
-            this.label3.Location = new System.Drawing.Point(15, 99);
+            this.label3.Location = new System.Drawing.Point(15, 132);
             this.label3.Margin = new System.Windows.Forms.Padding(1, 0, 1, 0);
             this.label3.Name = "label3";
             this.label3.Size = new System.Drawing.Size(49, 20);
@@ -149,7 +150,7 @@
             // 
             // TxtEmail
             // 
-            this.TxtEmail.Location = new System.Drawing.Point(120, 96);
+            this.TxtEmail.Location = new System.Drawing.Point(120, 129);
             this.TxtEmail.Margin = new System.Windows.Forms.Padding(1);
             this.TxtEmail.Name = "TxtEmail";
             this.TxtEmail.Size = new System.Drawing.Size(285, 27);
@@ -200,17 +201,27 @@
             // label8
             // 
             this.label8.AutoSize = true;
-            this.label8.Location = new System.Drawing.Point(15, 142);
+            this.label8.Location = new System.Drawing.Point(120, 9);
             this.label8.Name = "label8";
             this.label8.Size = new System.Drawing.Size(50, 20);
             this.label8.TabIndex = 18;
             this.label8.Text = "label8";
+            // 
+            // label9
+            // 
+            this.label9.AutoSize = true;
+            this.label9.Location = new System.Drawing.Point(15, 9);
+            this.label9.Name = "label9";
+            this.label9.Size = new System.Drawing.Size(94, 20);
+            this.label9.TabIndex = 19;
+            this.label9.Text = "Customer ID:";
             // 
             // AccountInfo
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 20F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(760, 272);
+            this.Controls.Add(this.label9);
             this.Controls.Add(this.label8);
             this.Controls.Add(this.buttonDeleteAccount);
             this.Controls.Add(this.TxtState);
@@ -257,5 +268,6 @@
         private TextBox TxtState;
         private Button buttonDeleteAccount;
         private Label label8;
+        private Label label9;
     }
 }
