322745f15075284455261ed784ceded4e6daa08e
Jordan Rabold <97548765+JordanRabold@users.noreply.github.com>
2022-08-24 13:38:54 -0700

    Added documentation to customer class

--- PizzaShopApp/Customer.cs
+++ PizzaShopApp/Customer.cs
@@ -10,30 +10,53 @@ namespace PizzaShopApp
 {
     public class Customer
     {
+        /// <summary>
+        /// An auto generated number assigned to a customer
+        /// </summary>
         [Key]
         public int CustomerID { get; set; }
 
+        /// <summary>
+        /// Customers legal First name
+        /// </summary>
         public string FirstName { get; set; }
 
+        /// <summary>
+        /// Customers legal Last name
+        /// </summary>
         public string LastName { get; set; }
 
+        /// <summary>
+        /// Customers Street Address to deliver to 
+        /// </summary>
         public string StreetAddress { get; set; }
 
+        /// <summary>
+        /// Customers State to deliver to 
+        /// </summary>
         public string State { get; set; }
 
+        /// <summary>
+        /// Customers City to deliver to 
+        /// </summary>
         public string City { get; set; }
 
-        [StringLength(5)]
+        /// <summary>
+        /// Customers Zipcode to deliver to
+        /// Must be 5 numbers long
+        /// </summary>
         public string ZipCode  { get; set; }
 
-        [Required]
-        [EmailAddress]
-        [StringLength(100)]
+        /// <summary>
+        /// Customers valid Email Address
+        /// Must be 50 characters or less
+        /// </summary>
         public string EmailAddress { get; set; }
 
-        [Required]
-        [StringLength(75, MinimumLength = 8)]
-        [DataType(DataType.Password)]
+        /// <summary>
+        /// Customers valid Password
+        /// Must be 8-50 characters long
+        /// </summary>
         public string Password { get; set; }
 
     }
