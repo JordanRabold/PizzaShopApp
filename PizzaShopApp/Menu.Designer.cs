503006c34c5eb1a36f9e5f2afb1d9cf95e3c5f08
Jordan Rabold <97548765+JordanRabold@users.noreply.github.com>
2022-08-30 17:04:54 -0700

    Implemented a simple ordering system(no CRUD)

--- PizzaShopApp/Menu.Designer.cs
+++ PizzaShopApp/Menu.Designer.cs
@@ -28,12 +28,346 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.groupBoxMenu = new System.Windows.Forms.GroupBox();
+            this.radioButtonDrinks = new System.Windows.Forms.RadioButton();
+            this.radioButtonSides = new System.Windows.Forms.RadioButton();
+            this.radioButtonWings = new System.Windows.Forms.RadioButton();
+            this.radioButtonPizza = new System.Windows.Forms.RadioButton();
+            this.groupBoxOptions = new System.Windows.Forms.GroupBox();
+            this.checkBox4 = new System.Windows.Forms.CheckBox();
+            this.checkBox3 = new System.Windows.Forms.CheckBox();
+            this.checkBox2 = new System.Windows.Forms.CheckBox();
+            this.checkBox1 = new System.Windows.Forms.CheckBox();
+            this.groupBoxTotal = new System.Windows.Forms.GroupBox();
+            this.textBoxOrderTotal = new System.Windows.Forms.TextBox();
+            this.textBoxTax = new System.Windows.Forms.TextBox();
+            this.textBoxSubtotal = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.labelSubtotal = new System.Windows.Forms.Label();
+            this.buttonPlaceOrder = new System.Windows.Forms.Button();
+            this.buttonBackToHome = new System.Windows.Forms.Button();
+            this.buttonAddToOrder = new System.Windows.Forms.Button();
+            this.buttonClearOrder = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.groupBoxMenu.SuspendLayout();
+            this.groupBoxOptions.SuspendLayout();
+            this.groupBoxTotal.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // groupBoxMenu
+            // 
+            this.groupBoxMenu.Controls.Add(this.label6);
+            this.groupBoxMenu.Controls.Add(this.label5);
+            this.groupBoxMenu.Controls.Add(this.label4);
+            this.groupBoxMenu.Controls.Add(this.label1);
+            this.groupBoxMenu.Controls.Add(this.radioButtonDrinks);
+            this.groupBoxMenu.Controls.Add(this.radioButtonSides);
+            this.groupBoxMenu.Controls.Add(this.radioButtonWings);
+            this.groupBoxMenu.Controls.Add(this.radioButtonPizza);
+            this.groupBoxMenu.Location = new System.Drawing.Point(12, 12);
+            this.groupBoxMenu.Name = "groupBoxMenu";
+            this.groupBoxMenu.Size = new System.Drawing.Size(250, 182);
+            this.groupBoxMenu.TabIndex = 0;
+            this.groupBoxMenu.TabStop = false;
+            this.groupBoxMenu.Text = "Menu";
+            // 
+            // radioButtonDrinks
+            // 
+            this.radioButtonDrinks.AutoSize = true;
+            this.radioButtonDrinks.Location = new System.Drawing.Point(6, 143);
+            this.radioButtonDrinks.Name = "radioButtonDrinks";
+            this.radioButtonDrinks.Size = new System.Drawing.Size(102, 24);
+            this.radioButtonDrinks.TabIndex = 3;
+            this.radioButtonDrinks.TabStop = true;
+            this.radioButtonDrinks.Text = "Soft Drinks";
+            this.radioButtonDrinks.UseVisualStyleBackColor = true;
+            this.radioButtonDrinks.CheckedChanged += new System.EventHandler(this.radioButtonDrinks_CheckedChanged);
+            // 
+            // radioButtonSides
+            // 
+            this.radioButtonSides.AutoSize = true;
+            this.radioButtonSides.Location = new System.Drawing.Point(6, 104);
+            this.radioButtonSides.Name = "radioButtonSides";
+            this.radioButtonSides.Size = new System.Drawing.Size(65, 24);
+            this.radioButtonSides.TabIndex = 2;
+            this.radioButtonSides.TabStop = true;
+            this.radioButtonSides.Text = "Sides";
+            this.radioButtonSides.UseVisualStyleBackColor = true;
+            this.radioButtonSides.CheckedChanged += new System.EventHandler(this.radioButtonSides_CheckedChanged);
+            // 
+            // radioButtonWings
+            // 
+            this.radioButtonWings.AutoSize = true;
+            this.radioButtonWings.Location = new System.Drawing.Point(6, 65);
+            this.radioButtonWings.Name = "radioButtonWings";
+            this.radioButtonWings.Size = new System.Drawing.Size(99, 24);
+            this.radioButtonWings.TabIndex = 1;
+            this.radioButtonWings.TabStop = true;
+            this.radioButtonWings.Text = "8pc Wings";
+            this.radioButtonWings.UseVisualStyleBackColor = true;
+            this.radioButtonWings.CheckedChanged += new System.EventHandler(this.radioButtonWings_CheckedChanged);
+            // 
+            // radioButtonPizza
+            // 
+            this.radioButtonPizza.AutoSize = true;
+            this.radioButtonPizza.Location = new System.Drawing.Point(6, 26);
+            this.radioButtonPizza.Name = "radioButtonPizza";
+            this.radioButtonPizza.Size = new System.Drawing.Size(90, 24);
+            this.radioButtonPizza.TabIndex = 0;
+            this.radioButtonPizza.TabStop = true;
+            this.radioButtonPizza.Text = "14\" Pizza";
+            this.radioButtonPizza.UseVisualStyleBackColor = true;
+            this.radioButtonPizza.CheckedChanged += new System.EventHandler(this.radioButtonPizza_CheckedChanged);
+            // 
+            // groupBoxOptions
+            // 
+            this.groupBoxOptions.Controls.Add(this.checkBox4);
+            this.groupBoxOptions.Controls.Add(this.checkBox3);
+            this.groupBoxOptions.Controls.Add(this.checkBox2);
+            this.groupBoxOptions.Controls.Add(this.checkBox1);
+            this.groupBoxOptions.Location = new System.Drawing.Point(301, 12);
+            this.groupBoxOptions.Name = "groupBoxOptions";
+            this.groupBoxOptions.Size = new System.Drawing.Size(250, 182);
+            this.groupBoxOptions.TabIndex = 1;
+            this.groupBoxOptions.TabStop = false;
+            this.groupBoxOptions.Text = "Options";
+            // 
+            // checkBox4
+            // 
+            this.checkBox4.AutoSize = true;
+            this.checkBox4.Location = new System.Drawing.Point(19, 144);
+            this.checkBox4.Name = "checkBox4";
+            this.checkBox4.Size = new System.Drawing.Size(101, 24);
+            this.checkBox4.TabIndex = 3;
+            this.checkBox4.Text = "checkBox4";
+            this.checkBox4.UseVisualStyleBackColor = true;
+            // 
+            // checkBox3
+            // 
+            this.checkBox3.AutoSize = true;
+            this.checkBox3.Location = new System.Drawing.Point(19, 104);
+            this.checkBox3.Name = "checkBox3";
+            this.checkBox3.Size = new System.Drawing.Size(101, 24);
+            this.checkBox3.TabIndex = 2;
+            this.checkBox3.Text = "checkBox3";
+            this.checkBox3.UseVisualStyleBackColor = true;
+            // 
+            // checkBox2
+            // 
+            this.checkBox2.AutoSize = true;
+            this.checkBox2.Location = new System.Drawing.Point(19, 65);
+            this.checkBox2.Name = "checkBox2";
+            this.checkBox2.Size = new System.Drawing.Size(101, 24);
+            this.checkBox2.TabIndex = 1;
+            this.checkBox2.Text = "checkBox2";
+            this.checkBox2.UseVisualStyleBackColor = true;
+            // 
+            // checkBox1
+            // 
+            this.checkBox1.AutoSize = true;
+            this.checkBox1.Location = new System.Drawing.Point(19, 26);
+            this.checkBox1.Name = "checkBox1";
+            this.checkBox1.Size = new System.Drawing.Size(101, 24);
+            this.checkBox1.TabIndex = 0;
+            this.checkBox1.Text = "checkBox1";
+            this.checkBox1.UseVisualStyleBackColor = true;
+            // 
+            // groupBoxTotal
+            // 
+            this.groupBoxTotal.Controls.Add(this.textBoxOrderTotal);
+            this.groupBoxTotal.Controls.Add(this.textBoxTax);
+            this.groupBoxTotal.Controls.Add(this.textBoxSubtotal);
+            this.groupBoxTotal.Controls.Add(this.label3);
+            this.groupBoxTotal.Controls.Add(this.label2);
+            this.groupBoxTotal.Controls.Add(this.labelSubtotal);
+            this.groupBoxTotal.Location = new System.Drawing.Point(12, 240);
+            this.groupBoxTotal.Name = "groupBoxTotal";
+            this.groupBoxTotal.Size = new System.Drawing.Size(333, 177);
+            this.groupBoxTotal.TabIndex = 2;
+            this.groupBoxTotal.TabStop = false;
+            this.groupBoxTotal.Text = "Order Total";
+            // 
+            // textBoxOrderTotal
+            // 
+            this.textBoxOrderTotal.Location = new System.Drawing.Point(125, 125);
+            this.textBoxOrderTotal.Name = "textBoxOrderTotal";
+            this.textBoxOrderTotal.ReadOnly = true;
+            this.textBoxOrderTotal.Size = new System.Drawing.Size(173, 27);
+            this.textBoxOrderTotal.TabIndex = 5;
+            // 
+            // textBoxTax
+            // 
+            this.textBoxTax.Location = new System.Drawing.Point(125, 83);
+            this.textBoxTax.Name = "textBoxTax";
+            this.textBoxTax.ReadOnly = true;
+            this.textBoxTax.Size = new System.Drawing.Size(173, 27);
+            this.textBoxTax.TabIndex = 4;
+            // 
+            // textBoxSubtotal
+            // 
+            this.textBoxSubtotal.Location = new System.Drawing.Point(125, 44);
+            this.textBoxSubtotal.Name = "textBoxSubtotal";
+            this.textBoxSubtotal.ReadOnly = true;
+            this.textBoxSubtotal.Size = new System.Drawing.Size(173, 27);
+            this.textBoxSubtotal.TabIndex = 3;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(27, 125);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(87, 20);
+            this.label3.TabIndex = 2;
+            this.label3.Text = "Order Total:";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(27, 83);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(82, 20);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Tax(6.50%):";
+            // 
+            // labelSubtotal
+            // 
+            this.labelSubtotal.AutoSize = true;
+            this.labelSubtotal.Location = new System.Drawing.Point(27, 44);
+            this.labelSubtotal.Name = "labelSubtotal";
+            this.labelSubtotal.Size = new System.Drawing.Size(68, 20);
+            this.labelSubtotal.TabIndex = 0;
+            this.labelSubtotal.Text = "Subtotal:";
+            // 
+            // buttonPlaceOrder
+            // 
+            this.buttonPlaceOrder.Location = new System.Drawing.Point(375, 323);
+            this.buttonPlaceOrder.Name = "buttonPlaceOrder";
+            this.buttonPlaceOrder.Size = new System.Drawing.Size(174, 29);
+            this.buttonPlaceOrder.TabIndex = 3;
+            this.buttonPlaceOrder.Text = "Place Order";
+            this.buttonPlaceOrder.UseVisualStyleBackColor = true;
+            this.buttonPlaceOrder.Click += new System.EventHandler(this.buttonPlaceOrder_Click);
+            // 
+            // buttonBackToHome
+            // 
+            this.buttonBackToHome.Location = new System.Drawing.Point(375, 365);
+            this.buttonBackToHome.Name = "buttonBackToHome";
+            this.buttonBackToHome.Size = new System.Drawing.Size(176, 29);
+            this.buttonBackToHome.TabIndex = 4;
+            this.buttonBackToHome.Text = "Back To Home";
+            this.buttonBackToHome.UseVisualStyleBackColor = true;
+            this.buttonBackToHome.Click += new System.EventHandler(this.buttonBackToHome_Click);
+            // 
+            // buttonAddToOrder
+            // 
+            this.buttonAddToOrder.Location = new System.Drawing.Point(375, 240);
+            this.buttonAddToOrder.Name = "buttonAddToOrder";
+            this.buttonAddToOrder.Size = new System.Drawing.Size(176, 29);
+            this.buttonAddToOrder.TabIndex = 5;
+            this.buttonAddToOrder.Text = "Add To Order";
+            this.buttonAddToOrder.UseVisualStyleBackColor = true;
+            this.buttonAddToOrder.Click += new System.EventHandler(this.buttonAddToOrder_Click);
+            // 
+            // buttonClearOrder
+            // 
+            this.buttonClearOrder.Location = new System.Drawing.Point(375, 282);
+            this.buttonClearOrder.Name = "buttonClearOrder";
+            this.buttonClearOrder.Size = new System.Drawing.Size(174, 29);
+            this.buttonClearOrder.TabIndex = 6;
+            this.buttonClearOrder.Text = "Clear Order";
+            this.buttonClearOrder.UseVisualStyleBackColor = true;
+            this.buttonClearOrder.Click += new System.EventHandler(this.buttonClearOrder_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(152, 30);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(44, 20);
+            this.label1.TabIndex = 4;
+            this.label1.Text = "$9.99";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(152, 69);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(44, 20);
+            this.label4.TabIndex = 5;
+            this.label4.Text = "$6.99";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(152, 108);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(44, 20);
+            this.label5.TabIndex = 6;
+            this.label5.Text = "$3.99";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(152, 145);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(44, 20);
+            this.label6.TabIndex = 7;
+            this.label6.Text = "$1.99";
+            // 
+            // Menu
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 20F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.ClientSize = new System.Drawing.Size(565, 450);
+            this.Controls.Add(this.buttonClearOrder);
+            this.Controls.Add(this.buttonAddToOrder);
+            this.Controls.Add(this.buttonBackToHome);
+            this.Controls.Add(this.buttonPlaceOrder);
+            this.Controls.Add(this.groupBoxTotal);
+            this.Controls.Add(this.groupBoxOptions);
+            this.Controls.Add(this.groupBoxMenu);
+            this.Name = "Menu";
             this.Text = "Menu";
+            this.groupBoxMenu.ResumeLayout(false);
+            this.groupBoxMenu.PerformLayout();
+            this.groupBoxOptions.ResumeLayout(false);
+            this.groupBoxOptions.PerformLayout();
+            this.groupBoxTotal.ResumeLayout(false);
+            this.groupBoxTotal.PerformLayout();
+            this.ResumeLayout(false);
+
         }
 
         #endregion
+
+        private GroupBox groupBoxMenu;
+        private RadioButton radioButtonDrinks;
+        private RadioButton radioButtonSides;
+        private RadioButton radioButtonWings;
+        private RadioButton radioButtonPizza;
+        private GroupBox groupBoxOptions;
+        private CheckBox checkBox4;
+        private CheckBox checkBox3;
+        private CheckBox checkBox2;
+        private CheckBox checkBox1;
+        private GroupBox groupBoxTotal;
+        private TextBox textBoxOrderTotal;
+        private TextBox textBoxTax;
+        private TextBox textBoxSubtotal;
+        private Label label3;
+        private Label label2;
+        private Label labelSubtotal;
+        private Button buttonPlaceOrder;
+        private Button buttonBackToHome;
+        private Button buttonAddToOrder;
+        private Button buttonClearOrder;
+        private Label label6;
+        private Label label5;
+        private Label label4;
+        private Label label1;
     }
 }
--- PizzaShopApp/Menu.cs
+++ PizzaShopApp/Menu.cs
@@ -16,5 +16,141 @@ namespace PizzaShopApp
         {
             InitializeComponent();
         }
+        List<double> subtotal = new List<double>();
+
+        private void radioButtonPizza_CheckedChanged(object sender, EventArgs e)
+        {
+            double pizza = 9.99;
+            if (radioButtonPizza.Checked)
+            {
+                ClearCheckBoxes();
+                checkBox1.Text = "Cheese";
+                checkBox2.Text = "Pepperoni";
+                checkBox3.Text = "Supreme";
+                checkBox4.Text = "Veggie";
+            }
+        }
+
+        private void radioButtonWings_CheckedChanged(object sender, EventArgs e)
+        {
+            double wings = 6.99;
+            if (radioButtonWings.Checked)
+            {
+                ClearCheckBoxes();
+                checkBox1.Text = "Buffalo";
+                checkBox2.Text = "Barbeque";
+                checkBox3.Text = "Parmesan Garlic";
+                checkBox4.Text = "Lemon Pepper Dry Rub";
+            }
+        }
+
+        private void radioButtonSides_CheckedChanged(object sender, EventArgs e)
+        {
+            double sides = 3.99;
+            if (radioButtonSides.Checked)
+            {
+                ClearCheckBoxes();
+                checkBox1.Text = "Cheesy Breadsticks";
+                checkBox2.Text = "Garlic Parmesan Bites";
+                checkBox3.Text = "Stuffed Cheesy Bread";
+                checkBox4.Text = "Mozzarella Sticks";
+            }
+            
+        }
+
+        private void radioButtonDrinks_CheckedChanged(object sender, EventArgs e)
+        {
+            double drinks = 1.99;
+            if (radioButtonDrinks.Checked)
+            {
+                ClearCheckBoxes();
+                checkBox1.Text = "Coca Cola";
+                checkBox2.Text = "Sprite";
+                checkBox3.Text = "Root Beer";
+                checkBox4.Text = "Dr. Pepper";
+            }
+        }
+
+        private void ClearCheckBoxes()
+        {
+            checkBox1.Checked = false;
+            checkBox2.Checked = false;
+            checkBox3.Checked = false;
+            checkBox4.Checked = false;
+        }
+
+        private void ClearTextBoxes()
+        {
+            textBoxOrderTotal.Clear();
+            textBoxSubtotal.Clear();
+            textBoxTax.Clear();
+        }
+
+        private void buttonAddToOrder_Click(object sender, EventArgs e)
+        {
+            double pizza = 9.99;
+            double wings = 6.99;
+            double sides = 3.99;
+            double drinks = 1.99;
+            double subtotalSum = 0;
+            double taxSum;
+            double orderTotal;
+            foreach(CheckBox c in groupBoxOptions.Controls)
+            {
+                if(radioButtonPizza.Checked && c.Checked)
+                {
+                    subtotal.Add(pizza);
+                }
+
+                if (radioButtonWings.Checked && c.Checked)
+                {
+                    subtotal.Add(wings);
+                }
+
+                if (radioButtonSides.Checked && c.Checked)
+                {
+                    subtotal.Add(sides);
+                }
+
+                if (radioButtonDrinks.Checked && c.Checked)
+                {
+                    subtotal.Add(drinks);
+                }
+            }
+
+            for (int i = 0; i < subtotal.Count; i++)
+            {
+                subtotalSum += subtotal[i];
+            }
+
+            taxSum = subtotalSum * .065;
+            orderTotal = subtotalSum + taxSum;
+
+            textBoxSubtotal.Text = subtotalSum.ToString("c");
+            textBoxTax.Text = taxSum.ToString("c");
+            textBoxOrderTotal.Text = orderTotal.ToString("c");
+        }
+
+        private void buttonPlaceOrder_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show("Thank's for placing an order!");
+            this.Hide();
+            HomePage home = new HomePage();
+            home.ShowDialog();
+        }
+
+        private void buttonBackToHome_Click(object sender, EventArgs e)
+        {
+            this.Hide();
+            HomePage home = new HomePage();
+            home.ShowDialog();
+        }
+
+        private void buttonClearOrder_Click(object sender, EventArgs e)
+        {
+            ClearCheckBoxes();
+            ClearTextBoxes();
+            subtotal.Clear();
+        }
     }
 }
